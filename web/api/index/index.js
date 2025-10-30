38ccf790672ba6f042c10391f27378e18cc0034f
v0-0-0 <gafforovaferide@gmail.com>
2025-10-30 19:37:01 +0500

    update index.js

--- web/api/index/index.js
+++ web/api/index/index.js
@@ -1,3 +1,3 @@
 // DS Desktop //
-document.getElementById("desktop").innerHTML = "<div id='ds_top' class='ds_top'></div><style>.ds_top{text-l";
+document.getElementById("desktop").innerHTML = "<div id='ds_top' class='ds_top'></div><style>.ds_top{text-align:center;c";
 document.getElementById("ds_top").innerHTML = "Huquqiy bilim kamolotga yetaklaydi. Huquqiy bilim kamolotga yetaklaydi.";
