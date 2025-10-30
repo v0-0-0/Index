0ad2de3d97bb1f79492aa6186e8a948cf5d0648a
v0-0-0 <gafforovaferide@gmail.com>
2025-10-30 19:10:55 +0500

    update index.js

--- web/api/index/index.js
+++ web/api/index/index.js
@@ -1,3 +1,3 @@
 // DS Desktop //
 document.getElementById("desktop").innerHTML = "<div id='ds_top'></div>";
-document.getElementById("ds_top").innerHTML = ""
+document.getElementById("ds_top").innerHTML = "Huquqiy bilim kamolotga y";
