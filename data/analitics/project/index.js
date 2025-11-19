b90bd9195c2be6de73db60f7230b59fb48348f64
v0-0-0 <gafforovaferide@gmail.com>
2025-11-19 09:00:32 +0500

    update index.js

--- data/analitics/project/index.js
+++ data/analitics/project/index.js
@@ -1,3 +1,4 @@
+document.getElementById("ds_top").innerHTML = "Ishga tushmoqda...";
 document.getElementById("").innerHTML = "";
 document.getElementById("").innerHTML = "";
 document.getElementById("").innerHTML = "";
